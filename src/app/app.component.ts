3bd22f536bd41d2faecf951fa90840647725a231
Joe Eames <joeeames@gmail.com>
2020-12-24 15:57:26 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lb-client';
+}
